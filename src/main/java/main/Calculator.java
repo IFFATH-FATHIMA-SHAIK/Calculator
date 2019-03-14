374dd4b4c7373f2fae7813011e0208f8b7f4ee89
User <User@Dell>
2019-03-14 20:48:57 +0530

    hjuhu

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -2,17 +2,18 @@ package main;
 import java.util.*; 
 
 public class Calculator {
-    public static void main(String[] args)
+    private static Scanner sc;
+	public static void main(String[] args)
     {
         System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
+        sc = new Scanner(System.in);
         int num1,num2;
-        num1 = inp.nextInt();
-        num2 = inp.nextInt();
-        int ans;
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        //int ans;
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
-        choose = inp.nextInt();
+        choose = sc.nextInt();
         switch (choose){
         case 1:
             System.out.println(add( num1,num2));
